d8783018b9dfe6d02d687a742fb4dc862b7f8b7a
VexMagic <89905295+VexMagic@users.noreply.github.com>
2024-01-24 15:09:57 +0100

    Player Movement

---
+++ Ritual/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rBody;
+    [SerializeField] private float moveSpeed;
+    private Vector2 movement;
+
+    void FixedUpdate()
+    {
+        //moves the player if a direction is inputed
+        if (movement != Vector2.zero)
+        {
+            Move();
+        }
+    }
+
+    private void OnMovement(InputValue value) //gets input from Unity Input System
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void Move()
+    {
+        rBody.MovePosition(rBody.position + movement * Time.fixedDeltaTime * moveSpeed);
+    }
+}
