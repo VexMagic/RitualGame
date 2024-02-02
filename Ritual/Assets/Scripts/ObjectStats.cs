16d06b7beb229bc87f728ed50761d6f733a4ea82
VexMagic <89905295+VexMagic@users.noreply.github.com>
2024-02-02 18:46:08 +0100

    added health bars

---
+++ Ritual/Assets/Scripts/ObjectStats.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectStats : MonoBehaviour
+{
+    private int health;
+    [SerializeField] private int maxHealth;
+
+    [SerializeField] private Slider healthBar;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.value = (float)health / (float)maxHealth;
+        if (health < 0)
+        {
+            Death();
+        }
+    }
+
+    public virtual void Death()
+    {
+        Debug.Log("Death");
+    }
+}
